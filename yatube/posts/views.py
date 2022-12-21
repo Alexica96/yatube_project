055e1e9bf50846d98ae36e55d1f4e8c5da9d62fc
Alexandr Lifanov <a.lifanow@yandex.ru>
2022-12-21 17:47:50 +0300

    Update, add title

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,29 @@
 from django.http import HttpResponse
-
+# Импортируем загрузчик.
+from django.template import loader
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = loader.get_template('posts\\index.html')
+    title = 'Это главная страница проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        'title': title,
+        'text': 'Главная страница',
+    }
+    #return render(request, template, context)
+    return HttpResponse(template.render(context, request))
 
 
 def groups_list(request):
-    return HttpResponse('Список групп')
+    template = loader.get_template('posts\\group_list.html')
+    title = 'Здесь будет информация о группах проекта Yatube'
+    # Словарь с данными принято называть context
+    context = {
+        'title': title,
+        'text': 'Страница групп',
+    }
+    # return render(request, template, context)
+    return HttpResponse(template.render(context, request))
 
 
 # В урл мы ждем парметр, и нужно его прередать в функцию для использования
